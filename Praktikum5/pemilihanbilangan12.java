6f08ec9a9ab0ff3c3880d528587f0ae676661c27
novalrnv <akuunoval@gmail.com>
2024-09-26 08:21:14 +0700

    Update : Modification used Ternary Operator

--- Praktikum5/pemilihanbilangan12.java
+++ Praktikum5/pemilihanbilangan12.java
@@ -8,10 +8,8 @@ public class pemilihanbilangan12{
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0){
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }else{
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil;
+        hasil = (angka % 2 == 0) ? "angka Genap" : "angka Ganjil";
+        System.out.println(angka + " termasuk " + hasil);
     }
 }
